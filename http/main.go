f5202d3e21ad39b8486a4ba8e6eceb65701958c6
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-05 16:42:19 -0400

    got a log writer going

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, error := http.Get("http://google.com")
 	if error != nil {
@@ -20,5 +22,18 @@ func main() {
 	// bs := make([]byte, 99999)
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
-	io.Copy(os.Stdout, resp.Body)
+
+	// Better way
+	// io.Copy(os.Stdout, resp.Body)
+
+	// Best way
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
